2944952bc38e0210068beeb919d4b928776eb24b
oguzhanuz <ouz13@ku.edu.tr>
2024-08-07 23:02:42 +0300

    moved constants to top

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup
 setup(
     name='sentiment_intent_classification_zeroshot_case',
     version='1.1',
+    install_requires=['transformers>=4.2', 'pyyaml', 'pandas', 'torch'],
     packages=['src'],
     url='',
     license='',
