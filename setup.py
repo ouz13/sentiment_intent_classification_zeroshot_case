d278e6dadb138413deb7de27f6de584a290c20ac
oguzhanuz <ouz13@ku.edu.tr>
2024-08-07 16:57:06 +0300

    packaging support

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='sentiment_intent_classification_zeroshot_case',
+    version='1.1',
+    packages=['src'],
+    url='',
+    license='',
+    author='oguzhanuz',
+    author_email='',
+    description=''
+)
